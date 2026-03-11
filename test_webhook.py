5d47b0816b265a4f53d7a2b75d14caae0ffaf17c
Anukul Chandra <anukulchandra5585@gmail.com>
2026-03-11 15:45:35 +0600

    n8n json file added

--- test_webhook.py
+++ test_webhook.py
@@ -1,12 +1,15 @@
 import requests
 
 
-user_requests ="Hi, How Are You, all ok ?"
+user_requests ="Can You Hear me ?"
 
 response_message = {"message":user_requests}
 
 url ="http://localhost:5678/webhook-test/df442a97-fe2a-4d06-99bf-282768a4c3e6"
 
-response = requests.post(url,json=user_requests)
+response = requests.post(url,json=response_message)
 
 print(response.status_code)
+#print(response.json())
+#print(response.text)
+print(response.json()[0]['output'])
